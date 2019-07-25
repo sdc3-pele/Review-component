d9c995fdc9732c803de07d9a50818833fa6662a3
Anish Patel <anish@Anishs-MacBook.local>
2019-07-24 17:18:25 -0700

    defined CRUD operations with knex

---
+++ database/models.js
@@ -0,0 +1,30 @@
+const knex = require('./db.js');
+
+//create
+//revObj is the object with key/value pairs for columns/value that will be udpated
+const newReview = (revObj) => {
+    return knex('reviews').insert(revObj)
+}
+//read
+//prodId is an int that represents the product Id
+const readReviews = (prodId) => {
+    return knex.select().table('reviews').where({listing_id: prodId})
+}
+//update
+//revId is the id for the review
+//revObj is the object with key/value pairs for columns/value that will be udpated
+const updateReview = (revId, revObj ) => {
+    return knex('reviews').where({id: revId}).update(revObj)
+}
+//delete
+//revId is the id for the review
+const delReview = (revId) => {
+    return knex('reviews').where({id: revId}).del()
+}
+
+module.exports = {
+    create: readReviews,
+    read: readReviews,
+    update: updateReview,
+    delete: delReview
+}
