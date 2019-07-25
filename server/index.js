8a16b8d33af0b8c32f55909fe991d16fd5fb7867
Anish Patel <anish@Anishs-MacBook.local>
2019-07-24 17:33:26 -0700

    complete API routes for crud operations

--- server/index.js
+++ server/index.js
@@ -18,8 +18,27 @@ app.get('/api/reviews/:id', (req, res) => {
   return models.read(req.params.id)
     .then(reviews => res.status(200).send(JSON.stringify(reviews)).end())
     .catch(err => res.status(400).send(err).end())
-    });
+  });
 
+app.delete('/api/reviews/:revid', (req, res) => {
+  return models.delete(req.params.revid)
+    .then(deletedRev => res.status(200).send(JSON.stringify(deletedRev)).end())
+    .catch(err => res.status(400).send(err).end())
+})
+
+app.post('/api/reviews/create/', (req, res) => {
+  let revObj = req.body
+  return models.create(revObj)
+    .then(newRev => res.status(200).send(JSON.stringify(newRev)).end())
+    .catch(err => res.status(400).send(err).end())
+})
+
+app.put('/api/reviews/:revid', (req, res) => {
+  let revObj = req.body;
+  return models.update(req.params.revid, revObj)
+    .then(updatedRev => res.status(200).send(JSON.stringify(updatedRev)).end())
+    .catch(err => res.status(400).send(err).end())
+})
 // app.get('/api/listings/', (req, res) => {
 //   // let id = ParseInt(req.params.id);
 //   getData((err, reviews) => {
