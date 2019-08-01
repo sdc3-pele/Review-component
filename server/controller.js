eec22e750de66b9b455627c5100b3ad9eefe0ca8
Anish Patel <anish@Anishs-MacBook.local>
2019-07-31 21:30:58 -0700

    fixed another samll error with host

--- server/controller.js
+++ server/controller.js
@@ -3,7 +3,7 @@ let host = process.env.DB_HOST || 'localhost';
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : `host`,
+      host : host,
       user : 'postgres',
       password : 'docker',
       database : 'reviews'
